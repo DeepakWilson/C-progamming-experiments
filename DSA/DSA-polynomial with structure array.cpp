cc8bc2313cfbce8f21068bef1a3bfb2bf584de46
Deepak Wilson <deepakwilson2018@gmail.com>
2024-08-22 11:55:02 +0530

    Update DSA-polynomial with structure array.cpp

--- DSA/DSA-polynomial with structure array.cpp
+++ DSA/DSA-polynomial with structure array.cpp
@@ -1,26 +1,25 @@
 #include<stdio.h>
-struct polynomial
+struct polynomial{
+ int expo;
+ int coeff;
+ }poly[100];
+void main()
 {
-  int coeff;
-  int expo;
-} poly[100];
-int main()
+ int i,d;
+ printf("Enter the number of terms");
+ scanf("%d",&d);
+ for(i=0;i<d;i++)
+ {
+ printf("Enter the coefficient and exponent of term %d",i+1);
+ scanf("%d%d",&poly[i].coeff,&poly[i].expo);
+ }
+printf("The polynomial is \n");
+for(i=0;i<d;i++)
 {
-	int d,i;
-	printf("Enter the no of terms");
-	scanf("%d",&d);
-	for(i=0;i<d;i++)
-	{
-	printf("Enter the coefficient and polynomial of term %d",i+1);
-	scanf("%d%d",&poly[i].coeff,&poly[i].expo);
-	}
-	
-	printf("The polynomial is\n");
-	for(i=0;i<d;i++)
-	{
-		printf("%dx^%d",poly[i].coeff,poly[i].expo);
-		if(poly[i].expo>0)
-		printf("+");
-		
-	}
+   printf("%dx^%d",poly[i].coeff,poly[i].expo);
+   if(poly[i].expo>0)
+   printf("+");
 }
+}
+
+ 
